cd189434bb40a3340ce83f0550e9f0a0d7bb5c19
william-rauwens-oliver <william.rauwens-oliver@laplateforme.io>
2025-06-01 22:02:56 +0200

    Bug fixed App.js./

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 // src/App.js
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import DashboardPage from './pages/DashboardPage';
@@ -12,6 +12,7 @@ function App() {
     <BrowserRouter>
       <MainLayout>
         <Routes>
+          <Route path="/" element={<Navigate to="/login" replace />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route
